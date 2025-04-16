9edad40cd3e51de3c90c160384baeeebf516b3d4
Jeremy Fowler <jembyfowler@gmail.com>
2025-04-16 12:51:57 -0600

    more data validation

--- Models/Car.cs
+++ Models/Car.cs
@@ -9,14 +9,13 @@ public class Car
   public DateTime UpdatedAt { get; set; }
   [MinLength(3), MaxLength(30)] public string Make { get; set; }
   public string Model { get; set; }
-
   [Range(1896, 2026)] public int Year { get; set; }
   public string Color { get; set; }
   // NOTE will default to null instead of 0 if no value is supplied
   public int? Price { get; set; }
   public int Mileage { get; set; }
   public string EngineType { get; set; }
-  public string ImgUrl { get; set; }
+  [Url, MaxLength(1000)] public string ImgUrl { get; set; }
   // NOTE will default to null instead of false if no value is supplied
   public bool? HasCleanTitle { get; set; }
   public string CreatorId { get; set; }
