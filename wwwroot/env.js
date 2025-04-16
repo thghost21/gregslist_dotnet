6c9b13268cebf9c41a28704bb9f7f7786d27e460
Jeremy Fowler <jembyfowler@gmail.com>
2025-04-16 12:54:05 -0600

    changed env

--- wwwroot/env.js
+++ wwwroot/env.js
@@ -1,3 +1,3 @@
-export const domain = 'dev-h63x8ohlbl1q2qkp.us.auth0.com'
-export const clientId = 'XX15k7a9Be1KE1Usl1aaOrDdzKJwvtUp'
-export const audience = 'https://jeremyisaraddude.com'
+export const domain = 'YOUR'
+export const clientId = 'STUFF'
+export const audience = 'GOES HERE'
